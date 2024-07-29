c2c5d3c6b0306800be90173f82578c3ff3a90fb9
Kumar Gaurav <k09gaurav@gmail.com>
2024-07-29 17:32:36 +0530

    week8

---
+++ Week_8/q.py
@@ -0,0 +1,20 @@
+#  Q2.2
+import pandas as pd
+import json
+def Q2_2():
+    data = json.loads(json_data.json)
+    flattened_data = pd.json_normalize(data)
+    df = pd.DataFrame(flattened_data)
+    print(df)
+    df.to_csv('flattened_data.csv', index=False)
+
+def Q2_3():
+    data = json.loads(json_data.json)
+    flattened_data = pd.json_normalize(data)
+    df = pd.DataFrame(flattened_data)
+    output_path = 'flattened_data.parquet'
+    df.to_parquet(output_path, engine='pyarrow', index=False)
+
+if __name__ == "__main__":
+    Q2_2()
+    Q2_3()
